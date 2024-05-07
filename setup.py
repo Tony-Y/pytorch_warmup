e5fb59ff5b3e3e22d41297c8cc9282a30061514c
Tony-Y <11532812+Tony-Y@users.noreply.github.com>
2024-05-07 04:06:19 +0000

    Add __version__ to __init__.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytorch-warmup",
-    version="0.1.1",
     author="Takenori Yamamoto",
     author_email="yamamoto.takenory@gmail.com",
     description="A PyTorch Extension for Learning Rate Warmup",
