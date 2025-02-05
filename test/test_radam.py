ee39b21112e71b495c00bf0f869aa9be212a734b
Tony-Y <11532812+Tony-Y@users.noreply.github.com>
2025-02-05 15:42:15 +0900

    Improve test_radam

--- test/test_radam.py
+++ test/test_radam.py
@@ -1,9 +1,28 @@
 import unittest
+import math
 import torch
 import pytorch_warmup as warmup
 
 from .test_base import _test_state_dict, _set_lr, _get_lr
 from .test_untuned import _test_optimizer
+
+
+# The expression of each warmup factor
+# offset: 6
+# beta2: 7/10
+# rho_inf: 17/3
+ewf = {
+    1: 3*math.sqrt(55795775461652507765)/126458170465,
+    2: 3*math.sqrt(118975550786574877912913153615)/2269508758199815,
+    3: 3*math.sqrt(364009685132320107701159663753)/2992977113632385,
+    4: 3*math.sqrt(258572826689968392763003617038979)/68225651323259287,
+    5: 3*math.sqrt(668289519821298522824847043230807053)/3138599717744915303,
+    6: 3*math.sqrt(60431582784117573249154657184784100939048735)/27879860688339331112605,
+    7: 3*math.sqrt(3668869686599344602586804992292010752258094185)/207030521845988349697045,
+    8: 3*math.sqrt(38610293903545800493859693214989542002076301625518651)/648745826249577848268496415,
+    9: 3*math.sqrt(12026080263946093429637752207887183661294840713819813)/353035787321509409011021039,
+    10: 3*math.sqrt(456865593113897246792694328842050091932272202605586577311)/67546148486329926220639511801,
+}
 
 
 class TestRAdam(unittest.TestCase):
@@ -24,8 +43,8 @@ class TestRAdam(unittest.TestCase):
         for step in range(1, 11):
             lr = [_get_lr(x['lr']) for x in optimizer.param_groups]
             print(f'{step} {lr}')
-            self.assertLess(lr[0], 0.5)
-            self.assertLess(lr[1], 0.1)
+            self.assertAlmostEqual(lr[0], 0.5 * ewf[step])
+            self.assertAlmostEqual(lr[1], 0.1 * ewf[step])
             optimizer.zero_grad()
             optimizer.step()
             with warmup_scheduler.dampening():
